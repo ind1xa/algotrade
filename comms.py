e749cd8323c3fd97d66eb6044de38f1cd4a4a74f
Ninjacornix <milanvrankic@gmail.com>
2023-04-01 17:54:56 +0200

    fix comm

--- comms.py
+++ comms.py
@@ -20,17 +20,13 @@ def main():
     g = np.zeros((len(keys), len(keys)))
     volumes = np.zeros((len(keys), len(keys)))
     for i, (key, value) in enumerate(pairs.items()):
+        x = key.split(",")[0][6:]
+        y = key.split(",")[1]
+        xi = keys.index(x)
+        yi = keys.index(y)
         if "close_" in key:
-            x = key.split(",")[0][6:]
-            y = key.split(",")[1]
-            xi = keys.index(x)
-            yi = keys.index(y)
             g[xi, yi] = value
         elif "volume_" in key:
-            x = key.split(",")[0][7:]
-            y = key.split(",")[1]
-            xi = keys.index(x)
-            yi = keys.index(y)
             volumes[xi, yi] = value
 
     start = "USDT"
@@ -47,63 +43,36 @@ def main():
     for i in range(middle, middle + 20):
         considered.append(startVolues[i])
 
+    
     for i in considered:
+        money = 1000
         checkValues = []
+        cof = i[1] / 10**8
         for j, key in enumerate(keys):
             if volumes[keys.index(i[2]), j] > 0:
                 checkValues.append(tuple([volumes[keys.index(i[2]), j], g[keys.index(i[2]), j], keys[j]]))
-        print(i[0] // 10**8, i[1] / 10**8, i[2])
-        print("#")
+
+        newValues = []
         for j in checkValues:
-            print(j[0] // 10**8, j[1] / 10**8, j[2])
-        print("---")
-        
-    
-    # sort by volume
-    """ startVolues.sort(key=lambda x: x[0], reverse=True)
+            cof *= j[1] / 10**8
+            for k, key in enumerate(keys):
+                if volumes[keys.index(j[2]), k] > 0:
+                    newValues.append(tuple([volumes[keys.index(j[2]), k], g[keys.index(j[2]), k], keys[k]]))
+            s = 0
+            for k in newValues:
+                if k[2] == start and j[2] != start:
+                    cof *= k[1] / 10**8
+                    s = cof
+                    if s > 1:
+                        print(i[2], i[1], i[0], j[2], j[1], j[0], k[2], k[1], k[0], s)
+                        print("first price: ", (int(i[1]) / 10**8) * (int(i[0]) / 10 **8))
+                        print("price: ", (int(i[1]) / 10**8) * (int(i[0]) / 10 **8) * (int(j[1]) / 10**8) * (int(j[0]) / 10 **8) * (int(k[1]) / 10**8) * (int(k[0]) / 10 **8))
 
-    inverseValues = []
-    for i, key in enumerate(keys):
-        if volumes[i, keys.index(start)] > 0:
-            inverseValues.append(tuple([volumes[i, keys.index(start)], g[i, keys.index(start)], keys[i]]))
-    
-    inverseValues.sort(key=lambda x: x[0], reverse=True)
+                    break
+            cof = i[1] / 10**8
 
-    firstValue = startVolues[len(startVolues) // 2]
-    coef *= firstValue[1] / 10**8
-    print(firstValue)
+        exit()
 
-    checkValue = firstValue[2]
-    print(checkValue)
-    print(coef)
-    checkValues = []
-    for i, key in enumerate(keys):
-        if volumes[keys.index(checkValue), i] > 0:
-            checkValues.append(tuple([volumes[keys.index(checkValue), i], g[keys.index(checkValue), i], keys[i]]))
-
-    checkValues.sort(key=lambda x: x[1], reverse=True)
-    print(checkValues)
-    marked = ""
-    for i in checkValues:
-        if i[2] != start:
-            coef *= i[1] / 10**8
-            marked = i[2]
-            break
-
-    print(marked)
-    print(coef)
-    finalVolues = []
-    for i, key in enumerate(keys):
-        if volumes[keys.index(marked), i] > 0:
-            finalVolues.append(tuple([volumes[keys.index(marked), i], g[keys.index(marked), i], keys[i]]))
-
-    finalVolues.sort(key=lambda x: x[0], reverse=True)
-    for i in finalVolues:
-        if i[2] == start:
-            coef *= i[1] / 10**8
-            break
-
-    print(coef) """
     print(time.time() - t)
 
     
